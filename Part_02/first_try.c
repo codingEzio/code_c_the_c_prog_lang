c821cfc037feeafd6191ab26d8706dce052e71b5
Ezio <assassinste@gmail.com>
2019-03-07 23:31:19 +0800

    Regular update (no really content yet)

---
+++ Part_02/first_try.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Go check the doc for complete references.
+    
+    Lots of good examples out there!
+        https://en.cppreference.com/w/c/types/limits
+    */
+    
+    return 0;
+}
